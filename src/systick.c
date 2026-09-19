c53caecfeba4bd9bd0b75ea58ceb8b73b2feeef9
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-09-19 14:26:35 +1000

    SYSTICK fixed thank god

--- src/systick.c
+++ src/systick.c
@@ -5,8 +5,17 @@
 // returns 0 if the count > 24Bits
 uint32_t configure_Systick(uint32_t micro_seconds)
 {
+    //Fixing the Reload situation:
+    uint32_t reload = (125 * micro_seconds) - 1; 
+
+    //Error checking for reload:
+    if (reload > 0xFFFFFF){
+        return 0;
+    }
+
     systick_hw->csr = 0; //Control Status register disabled
-    systick_hw->rvr = (12500 / micro_seconds); //Reload value register for determines the frequency of interrupts
+    systick_hw->rvr = reload;
+    //systick_hw->rvr = (12500 / micro_seconds); //Reload value register for determines the frequency of interrupts
     systick_hw->cvr = 0; //Current value register
     systick_hw->csr = 0x7; //enabling + turning on interpts 
 
