b4def12920e7bc2884774808849df47990896a17
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-09-21 12:56:06 +1000

    Implementing the pre-emptive scheduler

--- src/scheduler.c
+++ src/scheduler.c
@@ -10,18 +10,44 @@
 //-------------------MAIN FUNCTIONS------------------------------------------
 void scheduler(void) {
 //================================NON-PRE-EMPTIVE ROUND ROBIN=======================
-    if(Task_List == NULL) return; //There's nothing to run
+    // if(Task_List == NULL) return; //There's nothing to run
 
-    if(CurrentTCB == NULL || CurrentTCB->next == NULL) {
-        CurrentTCB = Task_List; //Wrapping to the head
-    }
-    else {
-        CurrentTCB = (volatile TCB_t *)CurrentTCB->next; //advancing with no priority
-    }
+    // if(CurrentTCB == NULL || CurrentTCB->next == NULL) {
+    //     CurrentTCB = Task_List; //Wrapping to the head
+    // }
+    // else {
+    //     CurrentTCB = (volatile TCB_t *)CurrentTCB->next; //advancing with no priority
+    // }
 //================================NON-PRE-EMPTIVE ROUND ROBIN=======================
 
 //================================PRE-EMPTIVE SCHEDULER=======================
+    if(Task_List == NULL) return; //There's nothing to run
 
+    //finding the highest priority in the Task list
+    uint8_t highest_priority = 0;
+    TCB_t *current = Task_List;
+
+    while (current != NULL){
+        if (current->priority > highest_priority){
+            highest_priority = current->priority;
+        }
+        current = current->next;
+    }
+
+    //Round robining based on highest priority:
+    TCB_t *start = (CurrentTCB == NULL) ? Task_List : (TCB_t *)CurrentTCB->next;
+    if (start == NULL){
+        start = Task_List;
+    }
+    current = start;
+    do{
+        if(current->priority == highest_priority){
+            CurrentTCB = current;
+            return;
+        }
+        current = (current->next == NULL) ? Task_List : current->next;
+    }
+    while (current != start);
 //================================PRE-EMPTIVE SCHEDULER=======================
 }
 
