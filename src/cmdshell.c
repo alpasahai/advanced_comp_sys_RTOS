4205396b657f6edf77cdf9626b8bc05a9b3fadb3
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-09-21 12:40:18 +1000

    Pretty cmd shell

--- src/cmdshell.c
+++ src/cmdshell.c
@@ -9,29 +9,37 @@
 #define MAX_LINE 64
 #define MAX_TOKENS 4
 
+//COLOURINGGGG
+#define PINK       "\x1b[38;5;205m"
+#define HOT_PINK   "\x1b[38;5;198m"
+#define LIGHT_PINK "\x1b[38;5;217m"
+#define ROSE       "\x1b[38;5;211m"
+#define MAGENTA    "\x1b[38;5;201m"
+#define RESET      "\x1b[0m"
+
 //-------------------HELPER FUNCTIONS------------------------------------------
 static void print_menu(void){
-    printf("-------------------------♡ ALPIE MAIN MENU ♡-------------------------\n");
-    printf("pt               + Printing the list of tasks\n");
-    printf("rt id            + Removing a task}\n");
-    printf("sd               + SHUT DOWN \n");
-    printf("mm               + Pulling Main Menu\n\n");
-    printf("blink <numflash> <priority>\n");
-    printf("count <position> <priority>            Suggest pos 0\n");
-    printf("flash <position> <priority>            Suggest pos 3\n");
-    printf("hexer <position> <priority>            Suggest pos 4\n");
-    printf("splat <position> <priority>            Suggest pos 7\n");
-    printf("-----------------------------------------------------------\n");    
+    printf(ROSE"-------------------------♡ ALPIE MAIN MENU ♡-------------------------\n"RESET);
+    printf(LIGHT_PINK"pt               + Printing the list of tasks\n"RESET);
+    printf(HOT_PINK"rt id            + Removing a task}\n"RESET);
+    printf(LIGHT_PINK"sd               + SHUT DOWN \n"RESET);
+    printf(HOT_PINK"mm               + Pulling Main Menu\n\n"RESET);
+    printf(MAGENTA"blink <numflash> <priority>\n"RESET);
+    printf(PINK"count <position> <priority>            Suggest pos 0\n"RESET);
+    printf(MAGENTA"flash <position> <priority>            Suggest pos 3\n"RESET);
+    printf(PINK"hexer <position> <priority>            Suggest pos 4\n"RESET);
+    printf(MAGENTA"splat <position> <priority>            Suggest pos 7\n"RESET);
+    printf(ROSE"-----------------------------------------------------------\n"RESET);    
 }
 
 static void print_task_list(void) {
-    printf("\n ♡ TASK LIST ♡ \n\n");
+    printf(ROSE"\n ♡ TASK LIST ♡ \n\n"RESET);
     TCB_t *current = Task_List;
     while(current != NULL) {
-        printf("\tID: %02u\tTASK NAME: %s\tPRIORITY: %u\n", current->id, current->name, current->priority);
+        printf(LIGHT_PINK"\tID: %02u\tTASK NAME: %s\tPRIORITY: %u\n", current->id, current->name, current->priority, RESET);
         current = current->next;
     }
-    printf("-----------------------------------------------------------\n");  
+    printf(ROSE"-----------------------------------------------------------\n"RESET);  
 }
 
 //-------------------MAIN FUNCTION------------------------------------------
@@ -42,7 +50,7 @@ void cmdShell(int dummy) {		// the dummy int is to keep consistent with the othe
     print_menu();
 
     while(1){
-        printf("alpie_shell > ");
+        printf(ROSE"alpie_shell > "RESET);
         readln(line, MAX_LINE);
 
         //tokening the spaces:
@@ -70,15 +78,15 @@ void cmdShell(int dummy) {		// the dummy int is to keep consistent with the othe
             }
             uint32_t id = (uint32_t)atoi(token[1]); //atoi is coverting char to int
             if(remove_Task(id)){
-                printf("Removed Task:  %u\n", id);
+                printf(LIGHT_PINK"Removed Task:  %u\n", id, RESET);
             }
             else{
-                printf("Task: %u not found...\n", id);
+                printf(LIGHT_PINK"Task: %u not found...\n", id, RESET);
             }
         }
         //SHUT DOWN
         else if (strcmp(token[0], "sd") == 0){
-            printf("++++++ SHUT DOWN ++++++\n");
+            printf(ROSE"++++++ SHUT DOWN ++++++\n"RESET);
             //Turning off sev_seg
             display_clear();
             memset(display_buffer, 0x00, 8); //clearing the memory
@@ -103,7 +111,7 @@ void cmdShell(int dummy) {		// the dummy int is to keep consistent with the othe
             else                                       add_Task(splat, arg, "splat", priority);
         }
         else {
-            printf("Unknown command '%s' — type mm' for main menu\n", token[0]);
+            printf(ROSE"Unknown command '%s' — type mm' for main menu\n", token[0], RESET);
         }
     }
 }
